427efccab0487516f5fa7ce8c1b5c4f73250c003
Gemma Mullan <20083580@mail.wit.ie>
2021-10-14 14:17:44 +0100

    Updating 'update bird'

---
+++ src/main/kotlin/org/mad/birdwatching/models/PlacemarkModel.kt
@@ -0,0 +1,5 @@
+package org.mad.birdwatching.models
+
+data class PlacemarkModel(var id: Long = 0,
+                          var title: String = "",
+                          var description: String = "")
