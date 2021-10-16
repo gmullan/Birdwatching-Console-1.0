3ce2a7149fd59e5e806a6bbb32a3809737bc6398
Gemma Mullan <20083580@mail.wit.ie>
2021-10-16 16:31:27 +0100

    Added File Helper.

---
+++ src/main/kotlin/org/mad/birdwatching/controllers/BirdController.kt
@@ -0,0 +1,93 @@
+package org.wit.birdwatching.controllers
+
+import mu.KotlinLogging
+import org.mad.birdwatching.models.BirdMemStore
+import org.mad.birdwatching.models.BirdModel
+import org.wit.bird.console.views.BirdView
+
+
+class BirdController {
+
+    val birds = BirdMemStore()
+    val birdView = BirdView()
+    val logger = KotlinLogging.logger {}
+
+    init {
+        logger.info { "Launching Bird Console App" }
+        println("Bird Kotlin App Version 1.0")
+    }
+
+    fun start() {
+        var input: Int
+
+        do {
+            input = menu()
+            when (input) {
+                1 -> add()
+                2 -> update()
+                3 -> list()
+                4 -> search()
+                -99 -> dummyData()
+                -1 -> println("Exiting App")
+                else -> println("Invalid Option")
+            }
+            println()
+        } while (input != -1)
+        logger.info { "Shutting Down Bird Console App" }
+    }
+
+
+    fun menu() :Int { return birdView.menu() }
+
+    fun add(){
+        var aBird = BirdModel()
+
+        if (birdView.addBirdData(aBird))
+            birds.create(aBird)
+        else
+            logger.info("Bird Not Added")
+    }
+
+    fun list() {
+        //birdView.listBirds(birds)
+        birdView.listBird(birds)
+    }
+
+    fun update() {
+
+        birdView.listBird(birds)
+        var searchId = birdView.getId()
+        val aBird = search(searchId)
+
+        if(aBird != null) {
+            if(birdView.updateBirdData(aBird)) {
+                birds.update(aBird)
+                //birdView.showBird(aBird)
+                birdView.showBirds(aBird)
+                logger.info("Bird Updated : [ $aBird ]")
+            }
+            else
+                logger.info("Bird Not Updated")
+        }
+        else
+            println("Bird Not Updated...")
+    }
+
+    fun search() {
+        val aBird = search(birdView.getId())!!
+       // birdView.showBird(aBird)
+        birdView.showBirds(aBird)
+    }
+
+
+    fun search(id: Long) : BirdModel? {
+        var foundBird = birds.findOne(id)
+        return foundBird
+    }
+
+    fun dummyData() {
+        birds.create(BirdModel(title = "New York New York", description = "So Good They Named It Twice"))
+        birds.create(BirdModel(title= "Ring of Kerry", description = "Some place in the Kingdom"))
+        birds.create(BirdModel(title = "Waterford City", description = "You get great Blaas Here!!"))
+    }
+}
---
+++ src/main/kotlin/org/mad/birdwatching/views/BirdView.kt
@@ -0,0 +1,88 @@
+package org.wit.bird.console.views
+
+import org.mad.birdwatching.models.BirdMemStore
+import org.mad.birdwatching.models.BirdModel
+//import org.mad.birdwatching.views.BirdView
+//import org.wit.bird.main.birds
+
+
+class BirdView {
+
+    fun menu() : Int {
+
+        var option : Int
+        var input: String?
+
+        println("MAIN MENU")
+        println(" 1. Add Bird")
+        println(" 2. Update Bird")
+        println(" 3. List All Birds")
+        println(" 4. Search Birds")
+        println("-1. Exit")
+        println()
+        print("Enter Option : ")
+        input = readLine()!!
+        option = if (input.toIntOrNull() != null && !input.isEmpty())
+            input.toInt()
+        else
+            -9
+        return option
+    }
+
+    fun listBird(birds : BirdMemStore) {
+        println("List All Birds")
+        println()
+        birds.logAll()
+        println()
+    }
+
+    fun showBirds(bird : BirdModel) {
+        if(bird != null)
+            println("Bird Details [ $bird ]")
+        else
+            println("Bird Not Found...")
+    }
+
+    fun addBirdData(bird : BirdModel) : Boolean {
+
+        println()
+        print("Enter a Title : ")
+        bird.title = readLine()!!
+        print("Enter a Description : ")
+        bird.description = readLine()!!
+
+        return bird.title.isNotEmpty() && bird.description.isNotEmpty()
+    }
+
+    fun updateBirdData(bird : BirdModel) : Boolean {
+
+        var tempTitle: String?
+        var tempDescription: String?
+
+        if (bird != null) {
+            print("Enter a new Title for [ " + bird.title + " ] : ")
+            tempTitle = readLine()!!
+            print("Enter a new Description for [ " + bird.description + " ] : ")
+            tempDescription = readLine()!!
+
+            if (!tempTitle.isNullOrEmpty() && !tempDescription.isNullOrEmpty()) {
+                bird.title = tempTitle
+                bird.description = tempDescription
+                return true
+            }
+        }
+        return false
+    }
+
+    fun getId() : Long {
+        var strId : String? // String to hold user input
+        var searchId : Long // Long to hold converted id
+        print("Enter id to Search/Update : ")
+        strId = readLine()!!
+        searchId = if (strId.toLongOrNull() != null && !strId.isEmpty())
+            strId.toLong()
+        else
+            -9
+        return searchId
+    }
+}
